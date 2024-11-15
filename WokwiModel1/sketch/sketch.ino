e10761e12352dfe9a641c045767b864df31d58bb
MaksymOleksiv <maksymoleksiv07@gmail.com>
2024-11-15 13:23:58 +0200

    fixed code

--- WokwiModel1/sketch/sketch.ino
+++ WokwiModel1/sketch/sketch.ino
@@ -1,25 +1,22 @@
+#define PIR 27
+#define BUZZER 26
+#define LED 2
+
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(3, INPUT);
-  pinMode(15, OUTPUT);
-  pinMode(21, INPUT);
+  Serial.begin(115200);
+  Serial.println("Hello, ESP32!");
+
+  pinMode(PIR, INPUT);
+  pinMode(BUZZER, OUTPUT);
+  pinMode(LED, OUTPUT);
 }
-int b = 0;
-int a = 0;
-bool run = false;
+
+boolean isOn = false;
+
 void loop() {
-  a = digitalRead(3);
-  b = digitalRead(21);
-  if (b) {
-    b = 0;
-    run = !run;
-    Serial.println(run);
-    delay(1000);
-  }
-  if (a && run){
-    a = 0;
-    tone(15, 100, 5000);
-    delay(5000);
-  }
+  Serial.println(digitalRead(PIR));
+  isOn = digitalRead(PIR);
+  digitalWrite(BUZZER, !isOn);
+  digitalWrite(LED, isOn);
+  
 }
